fe9aeaa8f91aa3732fdecdafc220b00cd76bb9a5
Hououin Kyouma <gauravmalik1729@gmail.com>
2025-01-29 19:31:05 +0530

    Add files via upload

---
+++ leet9.py
@@ -0,0 +1,32 @@
+'''
+Given an integer x, return true if x is a palindrome(Means: Read same left to right than right to left) and false otherwise.
+
+Example 1:
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+
+Example 2:
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+Example 3:
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+'''
+import sys
+
+x = 10
+# If negative it become impossible
+if x < 0:
+    print('False')
+    sys.exit()
+
+x= str(x)
+for i in range((len(x)//2 + 1)):
+    if x[i] != x[(i)*(-1) - 1]:
+        print('False')
+        sys.exit()
+print('True')
